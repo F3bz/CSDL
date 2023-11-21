66b9eb824b1ddd87ebdfa1e39b68fddc40a8a83d
F3bz <taidzvcl0982@gmail.com>
2023-11-22 03:46:31 +0700

    update

--- Quarantine/quanrantine/backend/admin/assets/inc/sidebar.php
+++ Quarantine/quanrantine/backend/admin/assets/inc/sidebar.php
@@ -107,32 +107,6 @@
                                     </li>
                                 </ul>
                             </li>
-
-                            <li>
-                                <a href="javascript: void(0);">
-                                    <i class="mdi mdi-cash-multiple "></i>
-                                    <span> Accounting </span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <ul class="nav-second-level" aria-expanded="false">
-                                    <li>
-                                        <a href="his_admin_add_acc.payable.php">Add Acc. Payable</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_manage_acc_payable.php">Manage Acc. Payable</a>
-                                    </li>
-                                    <hr>
-                                    <li>
-                                        <a href="his_admin_add_acc_receivable.php">Add Acc. Receivable</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_manage_acc_receivable.php">Manage Acc. Receivable</a>
-                                    </li>
-                                    <hr>
-                                    
-                                    
-                                </ul>
-                            </li>
                             <li>
                                 <a href="javascript: void(0);">
                                     <i class=" fas fa-funnel-dollar "></i>
@@ -254,28 +228,7 @@
                                         <a href="his_admin_surgery_records.php">Surgery Records</a>
                                     </li>
                                 </ul>
-                            </li>
-
-                            <li>
-                                <a href="javascript: void(0);">
-                                    <i class="mdi mdi-cash-refund "></i>
-                                    <span> Payrolls </span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <ul class="nav-second-level" aria-expanded="false">
-                                    <li>
-                                        <a href="his_admin_add_payroll.php">Add Payroll</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_manage_payrolls.php">Manage Payrolls</a>
-                                    </li>
-                                    <li>
-                                        <a href="his_admin_generate_payrolls.php">Generate Payrolls</a>
-                                    </li>
-                                </ul>
-                            </li>
-
-                         
+                            </li>       
                             <li>
                                 <a href="javascript: void(0);">
                                     <i class="fas fa-lock"></i>

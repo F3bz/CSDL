f837a88a51c40f3e97224d6f4f0becb776646a41
MinhSon2k3 <118090368+MinhSon2k3@users.noreply.github.com>
2023-11-25 20:15:38 +0700

    s

--- Quarantine/quanrantine/backend/admin/record_patient.php
+++ Quarantine/quanrantine/backend/admin/record_patient.php
@@ -32,12 +32,16 @@
             <?php
                $Patient_ID = $_GET['Patient_ID'];
 
-               $ret = "SELECT p.*, t.Test_ID, t.Test_Result, t.Test_Type, t.Test_Date, t.Cycle_Threshold, c.comorbidity_type, s.Symtomp_Type 
-               FROM patient p JOIN test t ON p.Patient_ID = t.Patient_ID
+               $ret = "SELECT p.*, t.Test_ID, t.Test_Result, t.Test_Type, t.Test_Date, t.Cycle_Threshold, c.comorbidity_type, s.Symtomp_Type, qcs.Name
+               FROM patient p 
+               JOIN test t ON p.Patient_ID = t.Patient_ID
+               JOIN nurse n ON p.Nurse_ID = n.Nurse_ID
+               JOIN quarantine_camp_staff qcs ON n.Quarantine_camp_Staff_ID = qcs.Quarantine_camp_Staff_ID
                LEFT JOIN (SELECT Patient_ID, GROUP_CONCAT(comorbidity_type) AS comorbidity_type
-               FROM comorbidity GROUP BY Patient_ID) c ON p.Patient_ID = c.Patient_ID 
-               LEFT JOIN (SELECT Patient_ID, GROUP_CONCAT(Symtomp_Type) AS symtomp_Type 
-               FROM symtomp GROUP BY Patient_ID) s ON p.Patient_ID = s.Patient_ID WHERE p.Patient_ID = ?";
+                          FROM comorbidity GROUP BY Patient_ID) c ON p.Patient_ID = c.Patient_ID 
+               LEFT JOIN (SELECT Patient_ID, GROUP_CONCAT(Symtomp_Type) AS Symtomp_Type 
+                          FROM symtomp GROUP BY Patient_ID) s ON p.Patient_ID = s.Patient_ID 
+               WHERE p.Patient_ID =?";
                
                $stmt = $mysqli->prepare($ret);
                $stmt->bind_param('i', $Patient_ID);
@@ -53,10 +57,10 @@
                 <div class="content">
 
                     <!-- Start Content-->
-                    <div class="container-fluid">
+                    <div class="container-fluid ">
 
                         <!-- start page title -->
-                        <div class="row">
+                        <div class="row mx-auto text-center">
                             <div class="col-12">
                                 <div class="page-title-box">
                                     <div class="page-title-right">
@@ -68,14 +72,14 @@
                         </div>
                         <!-- end page title -->
 
-                        <div class="row">
+                        <div class="row mx-auto text-center">
                             <div class="">
                                 <div class="card-box text-center">
                                     <img src="assets/images/users/patient.png" class="rounded-circle avatar-lg img-thumbnail"
                                         alt="profile-image">
 
                                     
-                                    <div class="text-left mt-3">
+                                    <div class="text-left mt-6">
                                         
                                         <p class="text-muted mb-2 font-13"><strong>Full Name :</strong> <span class="ml-2"><?php echo $row->Full_Name;?> </span></p>
                                         <p class="text-muted mb-2 font-13"><strong>Phone :</strong><span class="ml-2"><?php echo $row->Phone;?></span></p>
@@ -85,11 +89,15 @@
                                         <p class="text-muted mb-2 font-13"><strong>Test Type :</strong> <span class="ml-2"><?php echo $row->Test_Type;?></span></p>
                                         <p class="text-muted mb-2 font-13"><strong>Test Date :</strong> <span class="ml-2"><?php echo $row->Test_Date;?></span></p>
                                         <p class="text-muted mb-2 font-13"><strong>Cycle Threshold :</strong> <span class="ml-2"><?php echo $row->Cycle_Threshold;?></span></p>
-                                        <p class="text-muted mb-2 font-13"><strong>Comorbidity_Type:</strong> <span class="ml-2"><?php echo $row->comorbidity_type;?></span></p>
+                                           <!-- Câu lệnh điều kiện kiểm tra comorbidity -->
+                            <?php if ($row->comorbidity_type) : ?>
+                                <p class="text-muted mb-2 font-13"><strong>Comorbidity_Type:</strong> <span class="ml-2"><?php echo $row->comorbidity_type;?></span></p>
+                            <?php else : ?>
+                                <p class="text-muted mb-2 font-13"><strong>Comorbidity_Type:</strong> <span class="ml-2">N/A</span></p>
+                            <?php endif; ?>
                                         <p class="text-muted mb-2 font-13"><strong>Symtomp_Type:</strong> <span class="ml-2"><?php echo $row->Symtomp_Type;?></span></p>
-
-                                       
-
+                                        <p class="text-muted mb-2 font-13"><strong>Nurse:</strong> <span class="ml-2"><?php echo $row->Name;?></span></p>
+                                        <p class="text-muted mb-2 font-13"><strong>Room:</strong> <span class="ml-2"><?php echo $row->Room_ID;?></span></p>
 
 
                                     </div>

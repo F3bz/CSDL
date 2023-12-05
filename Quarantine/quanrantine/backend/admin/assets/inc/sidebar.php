364bd89ad17a866aea5eab053ed094ed7905752d
F3bz <taidzvcl0982@gmail.com>
2023-12-05 18:53:03 +0700

    Update

--- Quarantine/quanrantine/backend/admin/assets/inc/sidebar.php
+++ Quarantine/quanrantine/backend/admin/assets/inc/sidebar.php
@@ -79,20 +79,6 @@
                                     
                                 </ul>
                             </li>
-                            <li>
-                                <a href="javascript: void(0);">
-                                    <i class="fas fa-lock"></i>
-                                    <span> Password Resets </span>
-                                    <span class="menu-arrow"></span>
-                                </a>
-                                <ul class="nav-second-level" aria-expanded="false">
-                                    <li>
-                                        <a href="his_admin_manage_password_resets.php">Manage</a>
-                                    </li>
-                                                                        
-                                </ul>
-                            </li>
-
                         </ul>
 
                     </div>
